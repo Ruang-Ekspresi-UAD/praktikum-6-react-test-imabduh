c011624a6bfff7a17be82ea0aee460133683dd20
imabduh <imuhammadabduh@gmail.com>
2024-11-26 14:19:53 +0700

    menyelesaikan prak 6

--- test/counter.test.js
+++ test/counter.test.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Counter from './counter';
-import Display from './display';
 
+// counter.test.js
+
+// Contoh fungsi counter untuk diuji
+function createCounter() {
+    let count = 0;
+    return {
+      increment() {
+        count += 1;
+      },
+      decrement() {
+        count -= 1;
+      },
+      getCount() {
+        return count;
+      },
+    };
+  }
+  
+
+  test('Nilai awal counter adalah nol', () => {
+    const counter = createCounter();
+    expect(counter.getCount()).toBe(0);
+  });
+  
+
+  test('Increment meningkatkan nilai counter', () => {
+    const counter = createCounter();
+    counter.increment();
+    expect(counter.getCount()).toBe(1);
+  });
+  
+ 
+  test('Decrement menurunkan nilai counter', () => {
+    const counter = createCounter();
+    counter.increment(); // Tambah dulu
+    counter.decrement(); // Kurangi
+    expect(counter.getCount()).toBe(0);
+  });
+  
+  
--- test/latihan.test.js
+++ test/latihan.test.js
@@ -1,5 +1,21 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { Counter, Greeting, AlertButton } from './latihan';
 import '@testing-library/jest-dom';
-import React from 'react';
 
+test('penjumlahan sederhana', () => {
+    expect(1 + 2).toBe(3);
+  });
+  
+  test('Penjumlahan 1 + 2 sama dengan 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+  
+  // 2. Pengujian string
+  test('Menggabungkan string "Madang" dan "Sega"', () => {
+    expect('Madang' + 'Sega').toBe('MadangSega');
+  });
+  
+  // 3. Pengujian array
+  test('Array [1, 2, 3] mengandung angka 2', () => {
+    const arr = [1, 2, 3];
+    expect(arr).toContain(2);
+  });
+  
